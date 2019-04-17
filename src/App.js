21f112b05b75e99d540c8ebe6e1a1646a303b9a9
yaeSakura <363420113@qq.com>
2019-04-17 19:39:40 +0800

    添加env

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from 'react';
 
 class App extends Component {
   render() {
+    const env = process.env.NODE_ENV;
+    const v = process.env.REACT_APP_VERSION;
+    console.log(env);
+    console.log(v);
     return (
       <Fragment>
         <div>hello</div>
